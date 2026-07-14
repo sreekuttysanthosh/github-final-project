2b9263cc562126779b1eefe620bf733b669f233b
SREEKUTTY SANTHOSH <sreekuttysanthosh30@gmail.com>
2026-07-14 18:37:14 +0530

    simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -26,3 +26,14 @@
    s=$(echo "scale=2; $p * $t * $r / 100" | bc)
    echo "The simple interest is: "
    echo $s
+
+   echo "Enter the principal:"
+   read p
+   echo "Enter time period in years:"
+   read t
+   echo "Enter rate of interest per year:"
+   read r
+
+   s=$(echo "scale=2; $p * $t * $r / 100" | bc)
+   echo "The simple interest is: "
+   echo $s
